f1eb55a495feeaa7d7769683961afa5c8a0cda1c
catlinman <contact@catlinman.com>
2016-05-19 13:24:07 +0200

    Added bitfy.py script

---
+++ scripts/python/bitfy.py
@@ -0,0 +1,106 @@
+
+"""Bitfy
+Usage:
+    bitfy <command>
+          [--token=TOKEN]
+          [--url=URL]
+          [--request=URL]
+          [--file=PATH]
+          [--title=NAME]
+
+    bitfy (-h | --help)
+    bitfy (-v | --version)
+
+Options:
+    -h --help           Show this screen.
+    -v --version        Show version.
+
+    -t --token=<TOKEN>  Specifies and saves the token for OAuth connections.
+    -u --url=<URL>      Shorten the specified URL.
+    -r --request=<URL>  Get data from a request and then shorten all links.
+    -f --file=<PATH>    Get data from a file and then shorten all links.
+
+    -title --title=<NAME>   Identifier to use for generated links.
+
+Description:
+    Takes in an input, filters out all links and converts them to bit.ly links.
+"""
+
+import sys
+import os
+import json
+import re
+
+from docopt import docopt
+
+import requests
+
+
+def cli():
+    args = docopt(__doc__, version="bitfy 0.2")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    token = args["--token"]
+    links = []
+
+    if token:
+        f = open(os.path.join(script_dir, "token"), "w")
+        f.write(args["--token"])
+        f.close()
+
+    elif os.path.isfile(os.path.join(script_dir, "token")):
+        f = open(os.path.join(script_dir, "token"), "r")
+        token = f.read()
+        f.close()
+
+    else:
+        print("Please specify a token before performing any commands.")
+        return
+
+    if args["--url"]:
+        links.append(args["--url"])
+
+    if args["--file"]:
+        f = open(args["--file"], "r")
+        links = links + parse_links(f.read())
+        f.close()
+
+    if args["--request"]:
+        r = requests.get(args["--request"])
+
+        links = links + parse_links(r.content)
+
+    for link in links:
+        data = new_link(token, link)
+
+        if data["status_code"] != 200:
+            print("Error {} on {}: {}".format(data["status_code"], link, data["status_txt"]))
+            continue
+
+        domain = link.split("//")[-1].split("/")[0]
+
+        if args["--title"]:
+            edit_link(token, data["data"]["url"], "{} - {}".format(args["--title"], domain))
+            print("{} - {} | {}".format(args["--title"], domain, data["data"]["url"]))
+
+        else:
+            edit_link(token, data["data"]["url"], domain)
+            print("{} | {}".format(domain, data["data"]["url"]))
+
+
+def parse_links(data):
+    return re.findall(r'(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})', str(data))
+
+
+def new_link(token, link):
+    r = requests.get("https://api-ssl.bitly.com/v3/shorten?access_token={}&longUrl={}".format(token, link))
+    return json.loads(r.content.decode("utf-8"))
+
+
+def edit_link(token, link, title):
+    r = requests.get("https://api-ssl.bitly.com/v3/user/link_edit?access_token={}&link={}&edit=title&title={}".format(token, link, title))
+
+    return json.loads(r.content.decode("utf-8"))
+
+if __name__ == "__main__":
+    cli()
