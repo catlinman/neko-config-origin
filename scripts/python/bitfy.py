ce898303a56925e8c677462b4efaaf9715b20f60
catlinman <contact@catlinman.com>
2016-05-28 01:44:05 +0200

    Fixed command line arguments for bitfy.py

--- scripts/python/bitfy.py
+++ scripts/python/bitfy.py
@@ -1,11 +1,11 @@
 
 """Bitfy
 Usage:
-    bitfy.py --token=TOKEN
-    bitfy.py --url=URL
-    bitfy.py --request=URL
-    bitfy.py --file=PATH
-             [--title=NAME]
+    bitfy.py --token=TOKEN [--title=NAME]
+    bitfy.py --url=URL [--title=NAME]
+    bitfy.py --request=URL [--title=NAME]
+    bitfy.py --file=PATH [--title=NAME]
+
     bitfy.py (-h | --help)
     bitfy.py (-v | --version)
 
